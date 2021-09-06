509110f8eea17498d7f4f79ae40f3614648362d9
Dante Miller <mill8622@umm9020-h2yhx12.morris.umn.edu>
2021-09-05 21:23:12 -0500

    Fix Message in Wrap Content
    Reworded a hashtag message in the wrap content file

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -5,5 +5,5 @@ first_file=$1
 desired_name=$2
 # sets the variable second_file equal to the third argument passed to the script
 second_file=$3
-# wrap the first argument with the header.html and then places the contents of the footer.html in the second file
+# wrap the first argument with the header.html and footer.html then places in the second file
 cat "$desired_name"_"header.html" "$first_file" "$desired_name"_"footer.html" > "$second_file"
